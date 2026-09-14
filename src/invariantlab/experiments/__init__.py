97e1f0b145366baefde85edc71ae47eb23f3ccde
Alex Kurkar <124319262+akurkar07@users.noreply.github.com>
2026-09-15 00:48:19 +0100

    Export feedback replication runner

--- src/invariantlab/experiments/__init__.py
+++ src/invariantlab/experiments/__init__.py
@@ -1,5 +1,6 @@
 """Executable InvariantLab experiments."""
 
+from invariantlab.experiments.feedback_replication import run_feedback_replication
 from invariantlab.experiments.first_model import run_first_model_experiment
 
-__all__ = ["run_first_model_experiment"]
+__all__ = ["run_feedback_replication", "run_first_model_experiment"]
