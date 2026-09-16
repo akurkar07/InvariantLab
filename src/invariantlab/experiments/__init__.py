0fb868ed95d122e8487f37aa700f2c4cf5444606
Alex Kurkar <124319262+akurkar07@users.noreply.github.com>
2026-09-16 22:51:40 +0100

    feat(experiment): export generic repair runner

--- src/invariantlab/experiments/__init__.py
+++ src/invariantlab/experiments/__init__.py
@@ -2,5 +2,10 @@
 
 from invariantlab.experiments.feedback_replication import run_feedback_replication
 from invariantlab.experiments.first_model import run_first_model_experiment
+from invariantlab.experiments.generic_repair import run_generic_repair
 
-__all__ = ["run_feedback_replication", "run_first_model_experiment"]
+__all__ = [
+    "run_feedback_replication",
+    "run_first_model_experiment",
+    "run_generic_repair",
+]
